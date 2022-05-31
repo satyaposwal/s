c664261658f4032221108eb7c2cd0b8392a3069a
satyaposwal <100473179+satyaposwal@users.noreply.github.com>
2022-05-31 14:12:12 +0530

    Create promotion.groovy

---
+++ promotion.groovy
@@ -0,0 +1,23 @@
+pipeline {
+    agent any
+    stages {
+        stage('Pre-Build') {
+            steps {
+                echo 'Any Pre-requisite steps before the CodeBuild'
+            }
+        }
+        stage('Build on AWS CodeBuild') {
+            steps {
+                    awsCodeBuild buildSpecFile: 'buildspec.yml',
+                        credentialsId: 'sanyam',
+                        credentialsType: 'jenkins',
+                        projectName: 'satest',
+                        region: 'us-east-1',      
+                        sourceControlType: 'jenkins',
+                        sourceLocationOverride: 'sftp-bucket-poswal091993/art.zip',
+                        sourceTypeOverride: 'S3'
+            }
+        }
+    }
+}
+        
